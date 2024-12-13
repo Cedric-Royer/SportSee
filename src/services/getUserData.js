a22290ded086a709d95277cfe7973a68bba98014
Cédric Royer <cedricroyerpro@gmail.com>
2024-12-13 12:53:45 +0100

    fix getUserData mock

--- src/services/getUserData.js
+++ src/services/getUserData.js
@@ -2,24 +2,13 @@ import { USE_MOCK_DATA } from '../config';
 
 export const getUserData = async (userId) => {
     try {
-      if (USE_MOCK_DATA) {
-        // Assurer la conversion de userId en nombre avec parseInt
-        const userIdNumber = parseInt(userId, 10);  // Conversion explicite en nombre
-        const mockUser = mockData.data.find(user => user.id === userIdNumber);
-        
-        if (!mockUser) {
-          throw new Error("Utilisateur non trouvé dans les données mockées");
-        }
-        return formatUserData2(mockUser);
-      } else {
-        // Si USE_MOCK_DATA est false, appelle l'API réelle
-        const response = await fetch(`http://localhost:4000/user/${userId}`);
-        if (!response.ok) {
-          throw new Error("Erreur lors de la récupération des données de l'utilisateur");
-        }
-        const data = await response.json();
-        return formatUserData(data);
+      const url = USE_MOCK_DATA ? `../mocks/user/${userId}.json` : `http://localhost:4000/user/${userId}`
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des données de l'utilisateur");
       }
+      const data = await response.json();
+      return formatUserData(data);
     } catch (error) {
       console.error("Erreur lors de la récupération des données de l'utilisateur", error);
       throw error;
@@ -38,67 +27,3 @@ const formatUserData = (data) => {
         todayScore: data.data.todayScore || data.data.score || 0,
     };
 };
-
-const formatUserData2 = (data) => {
-    return {
-        firstName: data.userInfos.firstName || '',
-        nutritionData: {
-            calorieCount: data.keyData?.calorieCount || 0,
-            proteinCount: data.keyData?.proteinCount || 0,
-            carbohydrateCount: data.keyData?.carbohydrateCount || 0,
-            lipidCount: data.keyData?.lipidCount || 0,
-        },
-        todayScore: data.score || 0,
-    };
-};
-
-// Données mockées
-const mockData = {
-    "data": [
-      {
-        "id": 18,
-        "userInfos": {
-          "firstName": "Arthur",
-          "lastName": "Ratorez",
-          "age": 34
-        },
-        "score": 0.3,
-        "keyData": {
-          "calorieCount": 2500,
-          "proteinCount": 90,
-          "carbohydrateCount": 150,
-          "lipidCount": 120
-        }
-      },
-      {
-        "id": 19,
-        "userInfos": {
-          "firstName": "John",
-          "lastName": "Doe",
-          "age": 28
-        },
-        "score": 0.7,
-        "keyData": {
-          "calorieCount": 3000,
-          "proteinCount": 110,
-          "carbohydrateCount": 180,
-          "lipidCount": 150
-        }
-      },
-      {
-        "id": 20,
-        "userInfos": {
-          "firstName": "Sarah",
-          "lastName": "Smith",
-          "age": 25
-        },
-        "score": 0.5,
-        "keyData": {
-          "calorieCount": 2200,
-          "proteinCount": 80,
-          "carbohydrateCount": 130,
-          "lipidCount": 100
-        }
-      }
-    ]
-  };
